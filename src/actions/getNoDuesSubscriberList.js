00cb80638b74566ddbe718df9ab76eed1cd9a10b
Siddharth Singh Rana <siddharth_sr@cs.iitr.ac.in>
2023-04-07 16:36:05 +0530

    Add enrollment number search to greencard holders

--- src/actions/getNoDuesSubscriberList.js
+++ src/actions/getNoDuesSubscriberList.js
@@ -4,13 +4,14 @@ import { GET_NO_DUES_SUBSCRIBER_LIST } from "../constants/actions";
 
 import { subscriberListApi } from "../urls";
 
-export const getNoDuesSubscriberList = (page, start, end) => {
+export const getNoDuesSubscriberList = (page, start, end, enrolment_no) => {
   return (dispatch) => {
-    axios.get(`${subscriberListApi()}?page=${page}&start=${start}&end=${end}`).then((res) => {
-      dispatch({
-        type: GET_NO_DUES_SUBSCRIBER_LIST,
-        payload: res.data,
-      });
+    axios.get(`${subscriberListApi()}?page=${page}&start=${start}&end=${end}&enrolment_no=${enrolment_no}`)
+    .then((res) => {
+        dispatch({
+          type: GET_NO_DUES_SUBSCRIBER_LIST,
+          payload: res.data,
+        });
     });
   };
 };
